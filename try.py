129001bdc3611f18bbea3d7bcc21ae68bcfb64ac
phionah1601 <phionahchikwene@gmail.com>
2023-10-13 08:53:14 +0200

    del

--- try.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# Assuming you have a trained model named 'model_building'
-# ... your code to train the model ...
-
-# Save the model as a pickle file
-with open('sentiment_model.pkl', 'wb') as file:
-    pickle.dump(model building, file)
